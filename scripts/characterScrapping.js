a5bdbabc50463b6fb36b80d3ce08f48097b76cd8
Ludovic <ludovicmalot@icloud.com>
2024-01-27 18:15:19 +0100

    feat: Hutao

--- scripts/characterScrapping.js
+++ scripts/characterScrapping.js
@@ -5,7 +5,7 @@ const path = require('path');
 const axios = require('axios');
 
 const dataDirPath = './data';
-const fullLenght = 'gorou_055';
+const fullLenght = 'hutao_046';
 const charCode = fullLenght.split('_')[1];
 const charNameurl = fullLenght.split('_')[0];
 const charName = charNameurl.charAt(0).toUpperCase() + charNameurl.slice(1).toLowerCase();
@@ -28,9 +28,9 @@ const missingFields = {
 		weaponStance: `Character/${charName}/WeaponStance.webp`,
 	},
 	signatureArtifactSet: '',
-	signatureWeapon: 'RavenBow',
-	specialDish: 'VictoriousLegend',
-	tcgCharacterCard: '',
+	signatureWeapon: 'StaffOfHoma',
+	specialDish: 'GhostlyMarch',
+	tcgCharacterCard: 'HuTao',
 };
 
 async function getOutfit() {
