d8ebdf48385dc9115f009591337f8c6ff77495a3
Ludovic <ludovicmalot@icloud.com>
2024-01-27 18:28:35 +0100

    feat: kirara
    Former-commit-id: 717c11df5c9b0277cac6db279c448f4c551c2ea8

--- scripts/characterScrapping.js
+++ scripts/characterScrapping.js
@@ -5,10 +5,10 @@ const path = require('path');
 const axios = require('axios');
 
 const dataDirPath = './data';
-const fullLenght = 'keqing_042';
+const fullLenght = 'momoka_061';
 const charCode = fullLenght.split('_')[1];
 const charNameurl = fullLenght.split('_')[0];
-const charName = 'Keqing';
+const charName = 'Kirara';
 
 const honeyhunterworld = 'https://genshin.honeyhunterworld.com';
 const cheerio = require('cheerio');
@@ -28,9 +28,9 @@ const missingFields = {
 		weaponStance: `Character/${charName}/WeaponStance.webp`,
 	},
 	signatureArtifactSet: '',
-	signatureWeapon: 'LionsRoar',
-	specialDish: 'SurvivalGrilledFish',
-	tcgCharacterCard: 'Keqing',
+	signatureWeapon: '',
+	specialDish: 'EnergizingBento',
+	tcgCharacterCard: '',
 };
 
 async function getOutfit() {
