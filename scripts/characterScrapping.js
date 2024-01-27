b0fa8d602fe6b926f58f9d2f11eb32ef008cd294
Ludovic <ludovicmalot@icloud.com>
2024-01-27 20:11:36 +0100

    feat: Layla

--- scripts/characterScrapping.js
+++ scripts/characterScrapping.js
@@ -5,10 +5,10 @@ const path = require('path');
 const axios = require('axios');
 
 const dataDirPath = './data';
-const fullLenght = 'shinobu_065';
+const fullLenght = 'layla_074';
 const charCode = fullLenght.split('_')[1];
 const charNameurl = fullLenght.split('_')[0];
-const charName = 'KukiShinobu';
+const charName = 'Layla';
 
 const honeyhunterworld = 'https://genshin.honeyhunterworld.com';
 const cheerio = require('cheerio');
@@ -28,8 +28,8 @@ const missingFields = {
 		weaponStance: `Character/${charName}/WeaponStance.webp`,
 	},
 	signatureArtifactSet: '',
-	signatureWeapon: 'AmenomaKageuchi',
-	specialDish: 'OmuriceWaltz',
+	signatureWeapon: '',
+	specialDish: 'ExtravagantSlumber',
 	tcgCharacterCard: '',
 };
 
