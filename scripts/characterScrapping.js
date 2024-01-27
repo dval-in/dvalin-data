c066b7236fdff7dac2583ebd67c5bcf34a7c4532
Ludovic <ludovicmalot@icloud.com>
2024-01-27 18:07:16 +0100

    feat: freminet

--- scripts/characterScrapping.js
+++ scripts/characterScrapping.js
@@ -5,7 +5,7 @@ const path = require('path');
 const axios = require('axios');
 
 const dataDirPath = './data';
-const fullLenght = 'fischl_031';
+const fullLenght = 'freminet_085';
 const charCode = fullLenght.split('_')[1];
 const charNameurl = fullLenght.split('_')[0];
 const charName = charNameurl.charAt(0).toUpperCase() + charNameurl.slice(1).toLowerCase();
@@ -28,9 +28,9 @@ const missingFields = {
 		weaponStance: `Character/${charName}/WeaponStance.webp`,
 	},
 	signatureArtifactSet: '',
-	signatureWeapon: 'MitternachtsWaltz',
-	specialDish: 'DieHeiligeSinfonie',
-	tcgCharacterCard: 'Fischl',
+	signatureWeapon: 'TheBell',
+	specialDish: 'SeabirdsSojourn',
+	tcgCharacterCard: '',
 };
 
 async function getOutfit() {
