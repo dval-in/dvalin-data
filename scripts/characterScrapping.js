0ed91a606d28b532fb2a9af55c15ca3ad130aa63
Ludovic <ludovicmalot@icloud.com>
2024-01-27 20:20:00 +0100

    feat: Mona

--- scripts/characterScrapping.js
+++ scripts/characterScrapping.js
@@ -5,10 +5,10 @@ const path = require('path');
 const axios = require('axios');
 
 const dataDirPath = './data';
-const fullLenght = 'mika_080';
+const fullLenght = 'mona_041';
 const charCode = fullLenght.split('_')[1];
 const charNameurl = fullLenght.split('_')[0];
-const charName = 'Mika';
+const charName = 'Mona';
 
 const honeyhunterworld = 'https://genshin.honeyhunterworld.com';
 const cheerio = require('cheerio');
@@ -28,9 +28,9 @@ const missingFields = {
 		weaponStance: `Character/${charName}/WeaponStance.webp`,
 	},
 	signatureArtifactSet: '',
-	signatureWeapon: 'FavoniusLance',
-	specialDish: 'SurveyorsBreakfastSandwich',
-	tcgCharacterCard: '',
+	signatureWeapon: 'WineAndSong',
+	specialDish: 'DerWeisheitLetzterSchlussLife',
+	tcgCharacterCard: 'Mona',
 };
 
 async function getOutfit() {
