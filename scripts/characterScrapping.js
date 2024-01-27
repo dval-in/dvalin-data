64d77431066e1c73498d986e4d7bdcf98c57d52b
Ludovic <ludovicmalot@icloud.com>
2024-01-27 18:30:08 +0100

    feat: klee

--- scripts/characterScrapping.js
+++ scripts/characterScrapping.js
@@ -5,10 +5,10 @@ const path = require('path');
 const axios = require('axios');
 
 const dataDirPath = './data';
-const fullLenght = 'momoka_061';
+const fullLenght = 'klee_029';
 const charCode = fullLenght.split('_')[1];
 const charNameurl = fullLenght.split('_')[0];
-const charName = 'Kirara';
+const charName = 'Klee';
 
 const honeyhunterworld = 'https://genshin.honeyhunterworld.com';
 const cheerio = require('cheerio');
@@ -28,9 +28,9 @@ const missingFields = {
 		weaponStance: `Character/${charName}/WeaponStance.webp`,
 	},
 	signatureArtifactSet: '',
-	signatureWeapon: '',
-	specialDish: 'EnergizingBento',
-	tcgCharacterCard: '',
+	signatureWeapon: 'DodocoTales',
+	specialDish: 'FishFlavoredToast',
+	tcgCharacterCard: 'Klee',
 };
 
 async function getOutfit() {
