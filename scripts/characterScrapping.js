1ea1970ec775dae3c5c2913725b57ef33863bba7
Ludovic <ludovicmalot@icloud.com>
2024-01-27 20:16:44 +0100

    feat: Lyney

--- scripts/characterScrapping.js
+++ scripts/characterScrapping.js
@@ -5,10 +5,10 @@ const path = require('path');
 const axios = require('axios');
 
 const dataDirPath = './data';
-const fullLenght = 'linette_083';
+const fullLenght = 'liney_084';
 const charCode = fullLenght.split('_')[1];
 const charNameurl = fullLenght.split('_')[0];
-const charName = 'Lynette';
+const charName = 'Lyney';
 
 const honeyhunterworld = 'https://genshin.honeyhunterworld.com';
 const cheerio = require('cheerio');
@@ -28,9 +28,9 @@ const missingFields = {
 		weaponStance: `Character/${charName}/WeaponStance.webp`,
 	},
 	signatureArtifactSet: '',
-	signatureWeapon: 'FinaleOfTheDeep',
-	specialDish: 'ALeisurelySip',
-	tcgCharacterCard: 'Lynette',
+	signatureWeapon: 'TheFirstGreatMagic',
+	specialDish: 'CubicTricks',
+	tcgCharacterCard: 'Lyney',
 };
 
 async function getOutfit() {
