3af93022984e811895b37e11ed15a6d527941bf6
Ludovic <ludovicmalot@icloud.com>
2024-01-27 18:20:17 +0100

    feat: kaeya

--- scripts/characterScrapping.js
+++ scripts/characterScrapping.js
@@ -5,10 +5,10 @@ const path = require('path');
 const axios = require('axios');
 
 const dataDirPath = './data';
-const fullLenght = 'kazuha_047';
+const fullLenght = 'kaeya_015';
 const charCode = fullLenght.split('_')[1];
 const charNameurl = fullLenght.split('_')[0];
-const charName = 'KaedeharaKazuha';
+const charName = 'Kaeya';
 
 const honeyhunterworld = 'https://genshin.honeyhunterworld.com';
 const cheerio = require('cheerio');
@@ -28,9 +28,9 @@ const missingFields = {
 		weaponStance: `Character/${charName}/WeaponStance.webp`,
 	},
 	signatureArtifactSet: '',
-	signatureWeapon: 'KagotsurubeIsshin',
-	specialDish: '	AllWeatherBeauty',
-	tcgCharacterCard: 'KaedeharaKazuha',
+	signatureWeapon: 'HarbingerOfDawn',
+	specialDish: '	FruitySkewers',
+	tcgCharacterCard: 'Kaeya',
 };
 
 async function getOutfit() {
