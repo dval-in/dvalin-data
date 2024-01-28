d85a4d5676c971830f0b512eee77b8fa30dbdc93
Ludovic <ludovicmalot@icloud.com>
2024-01-28 23:18:41 +0100

    feat: Yanfei

--- scripts/characterScrapping.js
+++ scripts/characterScrapping.js
@@ -5,10 +5,10 @@ const path = require('path');
 const axios = require('axios');
 
 const dataDirPath = './data';
-const fullLenght = 'yae_058';
+const fullLenght = 'feiyan_048';
 const charCode = fullLenght.split('_')[1];
 const charNameurl = fullLenght.split('_')[0];
-const charName = 'YaeMiko';
+const charName = 'Yanfei';
 
 const honeyhunterworld = 'https://genshin.honeyhunterworld.com';
 const cheerio = require('cheerio');
@@ -28,9 +28,9 @@ const missingFields = {
 		weaponStance: `Character/${charName}/WeaponStance.webp`,
 	},
 	signatureArtifactSet: '',
-	signatureWeapon: 'KagurasVerity',
-	specialDish: 'FukuuchiUdon',
-	tcgCharacterCard: 'YaeMiko',
+	signatureWeapon: '',
+	specialDish: 'MyWay',
+	tcgCharacterCard: 'Yanfei',
 };
 
 async function getOutfit() {
