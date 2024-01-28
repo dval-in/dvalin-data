fe8e78ecd64364962c45d458dbfceabcb4f75f56
Ludovic <ludovicmalot@icloud.com>
2024-01-28 10:54:17 +0100

    feat: sayu
    Former-commit-id: 13ac1abad0d31d6238c4e5223558605dc9f7a515

--- scripts/characterScrapping.js
+++ scripts/characterScrapping.js
@@ -5,10 +5,10 @@ const path = require('path');
 const axios = require('axios');
 
 const dataDirPath = './data';
-const fullLenght = 'kokomi_054';
+const fullLenght = 'sayu_053';
 const charCode = fullLenght.split('_')[1];
 const charNameurl = fullLenght.split('_')[0];
-const charName = 'SangonomiyaKokomi';
+const charName = 'Sayu';
 
 const honeyhunterworld = 'https://genshin.honeyhunterworld.com';
 const cheerio = require('cheerio');
@@ -28,9 +28,9 @@ const missingFields = {
 		weaponStance: `Character/${charName}/WeaponStance.webp`,
 	},
 	signatureArtifactSet: '',
-	signatureWeapon: 'EverlastingMoonglow',
-	specialDish: 'AStunningStratagem',
-	tcgCharacterCard: 'SangonomiyaKokomi',
+	signatureWeapon: 'DebateClub',
+	specialDish: 'DizzinessBeGoneNoJutsuVersion20',
+	tcgCharacterCard: '',
 };
 
 async function getOutfit() {
