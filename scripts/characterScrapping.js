4c7b99efb2dd093f280444b6eb5e6427921442f3
Ludovic <ludovicmalot@icloud.com>
2024-01-28 11:01:59 +0100

    Thoma
    Former-commit-id: 2875fafeff8e4508ce3074ab3c42fda7b33ebf2c

--- scripts/characterScrapping.js
+++ scripts/characterScrapping.js
@@ -5,10 +5,10 @@ const path = require('path');
 const axios = require('axios');
 
 const dataDirPath = './data';
-const fullLenght = 'tartaglia_033';
+const fullLenght = 'tohma_050';
 const charCode = fullLenght.split('_')[1];
 const charNameurl = fullLenght.split('_')[0];
-const charName = 'Tartaglia';
+const charName = 'Thoma';
 
 const honeyhunterworld = 'https://genshin.honeyhunterworld.com';
 const cheerio = require('cheerio');
@@ -28,9 +28,9 @@ const missingFields = {
 		weaponStance: `Character/${charName}/WeaponStance.webp`,
 	},
 	signatureArtifactSet: '',
-	signatureWeapon: 'PolarStar',
-	specialDish: 'APrizeCatch',
-	tcgCharacterCard: 'Tartaglia',
+	signatureWeapon: 'KitainCrossSpear',
+	specialDish: 'Warmth',
+	tcgCharacterCard: '',
 };
 
 async function getOutfit() {
