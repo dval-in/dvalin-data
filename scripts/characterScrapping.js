f19a87169136e3d8be813ec522166330942a9f1d
Ludovic <ludovicmalot@icloud.com>
2024-01-28 23:16:31 +0100

    feat: yae Miko
    Former-commit-id: 511fc25d2c917b37e338392fb628c547a98d2a16

--- scripts/characterScrapping.js
+++ scripts/characterScrapping.js
@@ -5,10 +5,10 @@ const path = require('path');
 const axios = require('axios');
 
 const dataDirPath = './data';
-const fullLenght = 'xinyan_044';
+const fullLenght = 'yae_058';
 const charCode = fullLenght.split('_')[1];
 const charNameurl = fullLenght.split('_')[0];
-const charName = 'Xinyan';
+const charName = 'YaeMiko';
 
 const honeyhunterworld = 'https://genshin.honeyhunterworld.com';
 const cheerio = require('cheerio');
@@ -28,9 +28,9 @@ const missingFields = {
 		weaponStance: `Character/${charName}/WeaponStance.webp`,
 	},
 	signatureArtifactSet: '',
-	signatureWeapon: '',
-	specialDish: 'RockinRiffinChicken',
-	tcgCharacterCard: '',
+	signatureWeapon: 'KagurasVerity',
+	specialDish: 'FukuuchiUdon',
+	tcgCharacterCard: 'YaeMiko',
 };
 
 async function getOutfit() {
