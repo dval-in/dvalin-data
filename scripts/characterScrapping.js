1a27c52d940405ef07066c32c36a83db05edafdc
Ludovic <ludovicmalot@icloud.com>
2024-01-28 10:45:47 +0100

    feat: Raiden
    Former-commit-id: faa704b3a0cd636206585e0d9b1edcdb36a7ef3a

--- scripts/characterScrapping.js
+++ scripts/characterScrapping.js
@@ -5,10 +5,10 @@ const path = require('path');
 const axios = require('axios');
 
 const dataDirPath = './data';
-const fullLenght = 'qiqi_035';
+const fullLenght = 'shougun_052';
 const charCode = fullLenght.split('_')[1];
 const charNameurl = fullLenght.split('_')[0];
-const charName = 'Qiqi';
+const charName = 'RaidenShogun';
 
 const honeyhunterworld = 'https://genshin.honeyhunterworld.com';
 const cheerio = require('cheerio');
@@ -28,9 +28,9 @@ const missingFields = {
 		weaponStance: `Character/${charName}/WeaponStance.webp`,
 	},
 	signatureArtifactSet: '',
-	signatureWeapon: 'PrototypeRancour',
-	specialDish: 'NoTomorrow',
-	tcgCharacterCard: 'Qiqi',
+	signatureWeapon: 'EngulfingLightning',
+	specialDish: '',
+	tcgCharacterCard: 'RaidenShogun',
 };
 
 async function getOutfit() {
