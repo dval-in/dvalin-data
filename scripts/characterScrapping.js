71ff60dbca47dd469e8810eeb99571776a8892c4
Ludovic <ludovicmalot@icloud.com>
2024-01-28 11:00:36 +0100

    feat: tartaglia

--- scripts/characterScrapping.js
+++ scripts/characterScrapping.js
@@ -5,10 +5,10 @@ const path = require('path');
 const axios = require('axios');
 
 const dataDirPath = './data';
-const fullLenght = 'sucrose_043';
+const fullLenght = 'tartaglia_033';
 const charCode = fullLenght.split('_')[1];
 const charNameurl = fullLenght.split('_')[0];
-const charName = 'Sucrose';
+const charName = 'Tartaglia';
 
 const honeyhunterworld = 'https://genshin.honeyhunterworld.com';
 const cheerio = require('cheerio');
@@ -28,9 +28,9 @@ const missingFields = {
 		weaponStance: `Character/${charName}/WeaponStance.webp`,
 	},
 	signatureArtifactSet: '',
-	signatureWeapon: 'FavoniusCodex',
-	specialDish: 'NutritiousMealV593',
-	tcgCharacterCard: 'Sucrose',
+	signatureWeapon: 'PolarStar',
+	specialDish: 'APrizeCatch',
+	tcgCharacterCard: 'Tartaglia',
 };
 
 async function getOutfit() {
