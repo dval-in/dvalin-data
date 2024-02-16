16ff666060fcb4e017ac959b3c26379f1cc89c91
Samuel Giffard <samuel@giffard.co>
2024-02-16 16:18:56 +0100

    feat: Events: Add agent, in Docker
    Signed-off-by: Samuel Giffard <samuel@giffard.co>
    
    Former-commit-id: f75362a85347e685ec5cf553427d1b655c87b223 [formerly ca85117df7dae9536351a45e1527079320e8ee6c]
    Former-commit-id: 44cfef28fdad16578e34d1cdbb1b3bcbe07cd670

---
+++ dvalin-tools/dvalin_tools/agents/event_agent.py
@@ -0,0 +1,96 @@
+import asyncio
+from pathlib import Path
+
+from celery import Celery, chain
+from celery.schedules import crontab
+
+from dvalin_tools.lib.common import is_in_docker
+from dvalin_tools.lib.constants import DATA_DIR
+from dvalin_tools.lib.fs_lock import fs_lock
+from dvalin_tools.lib.languages import LANGUAGE_CODE_TO_DIR, LanguageCode
+from dvalin_tools.lib.settings import DvalinSettings
+from dvalin_tools.models.common import Game
+from dvalin_tools.models.events import EventFile, MessageType
+from dvalin_tools.scrapers.events import (
+    get_all_events,
+    update_event_files,
+    write_events,
+)
+
+settings = DvalinSettings()
+
+app = Celery(
+    "event_agent",
+    broker=settings.celery.broker_url,
+    backend=settings.celery.result_backend,
+)
+app.conf.broker_connection_retry_on_startup = True
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs) -> None:
+    sender.add_periodic_task(crontab(minute="*/1"), event_flow.s())
+
+
+def get_latest_event_file(data_dir: Path) -> Path:
+    event_dir_en = data_dir / LANGUAGE_CODE_TO_DIR[LanguageCode.ENGLISH] / "Event"
+    # This dir contains YYYY/MM/YYYY-MM.json files
+    # We want the latest one (so, we sort)
+    return sorted(event_dir_en.glob("*/*/*.json"))[-1]
+
+
+def get_last_event_post_id(data_dir: Path) -> int:
+    latest_event_file = get_latest_event_file(data_dir)
+    contents = latest_event_file.read_text(encoding="utf-8")
+
+    if not contents.strip():
+        raise ValueError(f"File {latest_event_file} is empty")
+
+    existing_events = EventFile.model_validate_json(contents)
+    latest_event = next(existing_events.iter_chronologically(reverse=True))
+
+    return int(latest_event.post_id)
+
+
+@app.task
+def event_flow() -> None:
+    chain(check_new_events.s(), process_new_events.s()).delay()
+
+
+@app.task
+def check_new_events() -> bool:
+    with fs_lock("check_new_events") as lock_acquired:
+        if not lock_acquired:
+            print("Another task already has the lock")
+            return False
+        print("Checking for new events")
+        return True
+
+
+@app.task
+def process_new_events(there_are_new_events: bool) -> None:
+    with fs_lock("process_new_events") as lock_acquired:
+        if not lock_acquired:
+            print("Another task already has the lock")
+            return
+
+        if there_are_new_events:
+            print("Processing new events")
+            asyncio.run(process_new_events_async())
+
+
+async def process_new_events_async() -> None:
+    print("Processing new events async")
+    data_dir = Path("/data") if is_in_docker() else DATA_DIR
+    latest_post_id = get_last_event_post_id(data_dir)
+    events = await get_all_events(
+        Game.GENSHIN_IMPACT, MessageType.INFO, limit=25, stop_at_post_id=latest_post_id
+    )
+    print(f"Retrieved {len(events)} new events")
+    modified_event_files = write_events(events, data_dir)
+    await update_event_files(modified_event_files)
+    print("New events processed")
+
+
+if __name__ == "__main__":
+    app.start()
