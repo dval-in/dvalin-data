096752d7e656297352fc637bae46ed4f1bce1743
Ludovic <ludovicmalot@icloud.com>
2024-05-07 18:36:15 +0200

    feat: better init script and path matching

---
+++ scripts/workflow/update_file.js
@@ -0,0 +1,47 @@
+import fs from 'fs';
+import path from 'path';
+import { toPascalCase, replaceRomanNumeralsPascalCased } from '../utils/stringUtils.js';
+const baseDir = path.resolve('../');
+
+// Construct the full path
+const filePath = path.join(baseDir, 'genshin-data/changed_files.txt');
+const file = fs.readFileSync(filePath, 'utf8');
+
+// filter all the lines not starting with src/data
+const lines = file.split('\n').filter(line => line.startsWith('src/data'));
+
+const folderMapping = {
+	'chinese-simplified': 'ZH-S',
+	'chinese-traditional': 'ZH-T',
+	english: 'EN',
+	french: 'FR',
+	german: 'DE',
+	indonesian: 'ID',
+	japanese: 'JP',
+	korean: 'KO',
+	portuguese: 'PT',
+	russian: 'RU',
+	spanish: 'ES',
+	thai: 'TH',
+	vietnamese: 'VI',
+	turkish: 'TR',
+	italian: 'IT',
+};
+
+for(let line of lines) {
+    const lang = line.split('/')[2];
+    const folder = line.split('/')[3];
+    if (folder === 'domains.json' || folder === 'domains') {
+        continue;
+    }
+    const file = line.split('/')[4].split('.')[0];
+    const newLang = folderMapping[lang];
+    const newFolder = toPascalCase(folder);
+    let newFile = toPascalCase(file);
+    if (newFolder === 'Achievements') {
+        newFile = replaceRomanNumeralsPascalCased(newFile);
+    }
+    const newPath = `data/${newLang}/${newFolder}/${newFile}.json`
+    console.log(newPath)
+}
+    
