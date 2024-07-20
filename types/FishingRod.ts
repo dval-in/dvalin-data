2f9fe97c9bd51da9238cc75d346e75b3fb58d802
Ludovic <ludovicmalot@icloud.com>
2024-07-20 16:05:11 +0800

    fx: FishingRod

--- types/FishingRod.ts
+++ types/FishingRod.ts
@@ -5,9 +5,11 @@ export type FishingRod = {
 	rarity: number;
 	source: string[];
 	version?: string;
-	processing?: {
-		id: string;
-		name: string;
-		amount: number;
-	};
+	processing?: Item[];
+};
+
+type Item = {
+	id: string;
+	name: string;
+	amount: number;
 };
